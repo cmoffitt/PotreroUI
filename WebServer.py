b27afba96d0e6a64e14b5ae8689d35b65250ffce
Colin Moffitt <colinmoffitt@gmail.com>
2024-06-05 11:36:53 -0600

    Added static image route

--- WebServer.py
+++ WebServer.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hello World from Potrero!</p>"
+
+@app.route("/image") 
+def serve_image(): 
+    return render_template('image.html')
